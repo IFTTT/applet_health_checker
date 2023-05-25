f3df1534bbe4e92c250e5c3bd443b7d56763eb6c
Aleksandr Furmanov <aleksandr@ifttt.com>
2023-05-25 09:37:02 -0600

    Sketching OAuth

--- app.rb
+++ app.rb
@@ -35,15 +35,11 @@ class App < Sinatra::Base
 
   get '/.well-known/ai-plugin.json' do
     content_type :json
-    ERB.
-      new(File.read('./.well-known/ai-plugin.json')).
-      result(binding)
+    erb File.read('./.well-known/ai-plugin.json')
   end
 
   get '/openapi.yaml' do
-    ERB.
-      new(File.read('./openapi.yaml')).
-      result(binding)
+    erb File.read('./openapi.yaml')
   end
 
   get '/logo.png' do
@@ -51,5 +47,32 @@ class App < Sinatra::Base
     File.read('./logo.png')
   end
 
- run! if app_file == $0
+  get '/oauth' do
+    puts "OAuth key value pairs from the ChatGPT Request: #{params}"
+    url = "#{params['redirect_uri']}?code=#{OPENAI_CODE}"
+    puts "URL: #{url}"
+    erb "<a href='<%=url%>'>Click to authorize</a>"
+  end
+
+  # Variables
+  OPENAI_CLIENT_ID = "ifttt_health_checker_plugin_client_id"
+  OPENAI_CLIENT_SECRET = "ifttt_health_checker_plugin_secret"
+  OPENAI_CODE = "abc123"
+  OPENAI_TOKEN = "def456"
+
+  # Route for handling the OAuth exchange
+  post '/auth/oauth_exchange' do
+    request_payload = JSON.parse(request.body.read)
+
+    puts "oauth_exchange request=#{request_payload}"
+
+    raise 'bad client ID' unless request_payload['client_id'] == OPENAI_CLIENT_ID
+    raise 'bad client secret' unless request_payload['client_secret'] == OPENAI_CLIENT_SECRET
+    raise RuntimeError, 'bad code' unless request_payload['code'] == OPENAI_CODE
+
+    content_type :json
+    { "access_token": OPENAI_TOKEN,  "token_type": "bearer" }.to_json
+  end
+
+  run! if app_file == $0
 end
